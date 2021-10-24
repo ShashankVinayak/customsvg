d60827a5593b52f9b1515bbcf4bc0eafaf0caf79
ShashankVinayak <shashank.devate@gmail.com>
2021-10-24 20:28:04 +0530

    first deployment

---
+++ _nuxt/static/1635084121/icons/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1635084121",layout:"icons",error:a,serverRendered:true,routePath:"\u002Ficons",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"b64cd6d9"}}}}(null));
