1805dc99d8de531b888935102efa6f1d5566609e
ShashankVinayak <shashank.devate@gmail.com>
2021-11-01 12:16:07 +0530

    update-1

---
+++ _nuxt/static/1635747514/icons/entypo/thermometer/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1635747514",layout:"icons",error:a,serverRendered:true,routePath:"\u002Ficons\u002Fentypo\u002Fthermometer",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"362018cd"}}}}(null));
---
+++ _nuxt/static/1635747514/icons/map/art-gallery/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1635747514",layout:"icons",error:a,serverRendered:true,routePath:"\u002Ficons\u002Fmap\u002Fart-gallery",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"362018cd"}}}}(null));
