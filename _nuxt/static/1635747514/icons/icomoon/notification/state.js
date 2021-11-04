7c8feb470c816235415984b5529f03294f931feb
ShashankVinayak <shashank.devate@gmail.com>
2021-11-04 14:27:17 +0530

    update-1

--- _nuxt/static/1635747514/icons/icomoon/notification/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1635747514",layout:"icons",error:a,serverRendered:true,routePath:"\u002Ficons\u002Ficomoon\u002Fnotification",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"362018cd"}}}}(null));
